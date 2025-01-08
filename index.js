413490bd7586b122355357eef60dcfb65288c0e3
Rakib <bsse1320@iit.du.ac.bd>
2025-01-08 23:58:38 +0600

    Alhamdulillah

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log('Hello world')
+const app = require("./app");
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port http://localhost:${port}`);
+});
