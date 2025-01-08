3f8c208cbdff735440716795cae99d3ae2406198
Rakib <bsse1320@iit.du.ac.bd>
2025-01-09 00:11:06 +0600

    Ok

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello, Rakib World this is a practice for CICD!");
+  res.send("Hello, World this is a practice for CICD!");
 });
 
 module.exports = app;
